c7246e984b2591d11e063beba2838ba4acc59dbd
myeongjjj <shyaudwn1@naver.com>
2024-04-04 20:40:27 +0900

    초기셋팅

---
+++ src/main/java/yurp/YuRpProjApplication.java
@@ -0,0 +1,13 @@
+package yurp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuRpProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuRpProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yurp/YuRpProjApplicationTests.java
@@ -0,0 +1,13 @@
+package yurp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuRpProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
