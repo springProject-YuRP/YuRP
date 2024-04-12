a0d6ba34195359388898688e49b9c522254fec50
Guri <pretzel2m5@gmail.com>
2024-04-12 13:24:58 +0900

    as/notice 보완

--- src/main/java/yurp/model/AsMapper.java
+++ src/main/java/yurp/model/AsMapper.java
@@ -12,7 +12,8 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface AsMapper {
 
-	@Select("SELECT ascenter.*, a.s_name as sName from ascenter "
+	//------공통
+	@Select("SELECT ascenter.*, a.s_name as sName, a.addr as addr from ascenter "
 			+ "INNER JOIN store AS a ON a.s_code = ascenter.s_code")
 	List<AsDTO> list(AsDTO dto);
 	
@@ -23,6 +24,7 @@ public interface AsMapper {
 			+ "WHERE ascenter.a_no = #{aNo}")
 	AsDTO detail(int aNo);
 	
+	//------본사
 	@Update("update ascenter set "
 			+ "res_con = #{resCon}, as_price = #{asPrice}, as_res = #{asRes}, as_prog = #{asProg} "
 			+ "where a_no = #{aNo}")
@@ -34,6 +36,10 @@ public interface AsMapper {
 			+ "(#{asNum},#{sCode},#{asName},#{asTel},#{pCode},#{asNote},#{purDate})")
 	int storeInsert(AsDTO dto);
 	
+	// 접수번호 자동
+	@Select("SELECT as_num+1 from ascenter ORDER BY a_no DESC LIMIT 1")
+	int asNumSelect(AsDTO dto);
+	
 	@Update("update ascenter set "
 			+ "as_name = #{asName}, as_tel = #{asTel}, p_code = #{pCode}, as_note = #{asNote}, pur_date = #{purDate} "
 			+ "where a_no = #{aNo}")
@@ -44,9 +50,39 @@ public interface AsMapper {
 	
 	
 	//------검색
-	//전체 매장명
-	@Select("SELECT s_name FROM store")
+	//매장
+	@Select("SELECT * FROM store")
 	List<AsDTO> sNames();
+	
+	//전체 매장
+	@Select({
+		" <script> "
+		," SELECT * FROM store "
+		, " <where> "
+		, " 	<if test='sName != null'> "
+		, " 		AND s_name like concat('%', #{sName}, '%')"
+		, " 	</if> "
+		, " 	<if test='sCode != null'> "
+		, " 		AND s_code like concat('%', #{sCode}, '%')"
+		, " 	</if> "
+		, " </where> "
+		, " </script> "})
+	List<AsDTO> sCodeSearch(AsDTO dto);
+	
+	//전체 상품
+	@Select({
+		" <script> "
+		," SELECT * FROM product "
+		, " <where> "
+		, " 	<if test='pName != null'> "
+		, " 		AND p_name like concat('%', #{pName}, '%')"
+		, " 	</if> "
+		, " 	<if test='pCode != null'> "
+		, " 		AND p_code like concat('%', #{pCode}, '%')"
+		, " 	</if> "
+		, " </where> "
+		, " </script> "})
+	List<AsDTO> pCodeSearch(AsDTO dto);
 	
 	
 	@Select({
@@ -54,27 +90,29 @@ public interface AsMapper {
 		," SELECT ascenter.*, a.s_name as sName from ascenter "
 		, "INNER JOIN store AS a ON a.s_code = ascenter.s_code"
 		, " <where> "
-		, " 	<choose> "
-		, " 		<when test='start != null and start != \"\" and end != null and end != \"\"' > "
-		, "						reg_date BETWEEN #{start} and #{end}"
-		, " 		</when> "
-		, " 		<when test='sName != null'> "
-		, " 			AND a.s_name = #{sName} "
-		, " 		</when> "
-		, " 		<when test='sName != null'> "
-		, " 			AND a.s_name = #{sName} "
-		, " 		</when> "
-		, " 		<when test='asRes != null'> "
-		, " 			AND as_res = #{asRes} "
-		, " 		</when> "
-		, " 		<when test='asProg != null'> "
-		, " 			AND as_prog = #{asProg} "
-		, " 		</when> "
-		, " 		<when test='asNum != null'> "
-		, " 			AND as_num like #{asNum} "
-		, " 		</when> "
-		, " 	</choose> "
+		, " 	<if test='start != null and start != \"\"' > "
+		, "			AND reg_date >= #{start} "
+		, " 	</if> "
+		, " 	<if test='end != null and end != \"\"' > "
+		, "			AND reg_date &lt;= #{end} "
+		, " 	</if> "
+		, " 	<if test='start != null and start != \"\" and end != null and end != \"\"' > "
+		, "			AND reg_date BETWEEN #{start} and #{end} "
+		, " 	</if> "
+		, " 	<if test='sName != null'> "
+		, " 		AND a.s_name = #{sName} "
+		, " 	</if> "
+		, " 	<if test='asRes != null'> "
+		, " 		AND as_res = #{asRes} "
+		, " 	</if> "
+		, " 	<if test='asProg != null'> "
+		, " 		AND as_prog = #{asProg} "
+		, " 	</if> "
+		, " 	<if test='asNum != null'> "
+		, " 		AND as_num like concat('%', #{asNum}, '%')"
+		, " 	</if> "
 		, " </where> "
 		, " </script> "})
 	List<AsDTO> listPname(AsDTO dto);
+
 }
